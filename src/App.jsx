28e6895da6df4bed128e84179d49cf23a254d7d0
Hassan-Kirmani9 <kirmanihassan153@gmail.com>
2025-01-24 19:14:56 +0500

    NF

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import { NotFound } from "./components/404page";
 import Benefits from "./components/Benefits";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -10,7 +11,8 @@ import Testimonial from "./components/Testimonial";
 const App = () => {
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+    <NotFound/>
+      {/* <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
         <Benefits />
@@ -18,9 +20,9 @@ const App = () => {
         <Roadmap />
         <Testimonial/>
         <Footer />
-      </div>
+      </div> */}
 
-      <ButtonGradient />
+      {/* <ButtonGradient /> */}
     </>
   );
 };
